c7f35aa951d53c2c8fa00aace861fabd800e3fd2
Tatiana <kutuzova.tanya@mail.ru>
2018-07-24 11:51:24 +0300

    update integration

--- src/network/integration_network.py
+++ src/network/integration_network.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import pandas as pd
 import community
+import numpy as np
 
 print("Integration of external dataset")
 data_path = "E:\Projects\MBA_retail\\tmp"
@@ -11,8 +12,6 @@ dataset = pd.read_csv('{0}\\change_train.csv'.format(dataset_path))
 number_of_orders = len(dataset)
 lbls = list(dataset)
 dataset = dataset.values
-
-
 
 name = "change_train"
 graph = pd.read_csv('{0}\\{1}_graph.csv'.format(data_path, name))
@@ -33,45 +32,56 @@ external_dataset = external_dataset.values
 number_of_ex_orders = len(external_dataset)
 number_of_products = len(external_dataset[0])
 m = dataset[len(dataset)-1][0]
-#average_clust = nx.average_clustering(network)
-
-N_edges_L = nx.number_of_edges(network)
-N_nodes_N = nx.number_of_nodes(network)
-average_degree_k = float(N_edges_L)/ float(N_nodes_N)
-print("Average degree", average_degree_k)
+dict_orders_orig = {}
 counter_nodes = 0
+degrees = []
+inf = (nx.info(network))
+degrees.append(float(inf[-7:])/2.0)
+print(float(inf[-7:]))
 for j in range(number_of_ex_orders):
     print(j+1, '/', number_of_ex_orders)
     counter_edges = 0
     for i in range(number_of_orders):
-        weight_0 = 0
         weight_1 = 0
-        for p in range(1, number_of_products):
-            #if dataset[i][p] == external_dataset[j][p] == 0:
-                #weight_0 += 1
-            if dataset[i][p] == external_dataset[j][p] == 1:
-                weight_1 += 1
+        weight_1 = sum(dataset[i] & external_dataset[j])
+
         if weight_1 != 0:
-            weight = weight_0+weight_1
+            weight = weight_1
             network.add_edge(dataset[i][0], m+external_dataset[j][0], weight=weight)
-            counter_edges += 1
-            N_edges_L = nx.number_of_edges(network)
-            N_nodes_N = nx.number_of_nodes(network)
-            average_degree = float(N_edges_L) / float(N_nodes_N)
-            if average_degree_k - average_degree < 0.007:
-                network.remove_edge(dataset[i][0], m + external_dataset[j][0])
-                counter_edges -= 1
-    if counter_edges != 0:
+        if j == 0:
+            dict_orders_orig.update({dataset[i][0]: i})
+    inf = (nx.info(network))
+    degrees.append(float(inf[83:90]))
+    print(degrees[-2], degrees[-1])
+    if(abs(degrees[-2] - degrees[-1]) > 2):
+        network.remove_node(m + external_dataset[j][0])
+        del degrees[-1]
+    else:
         counter_nodes += 1
+
     print(counter_nodes)
 
 print(counter_nodes, 'nodes are integrated')
 integrated_dataset = []
-for i in list(network.nodes):
-    if i <= m:
-        integrated_dataset.append(dataset[i])
-    if i > m:
-        integrated_dataset.append(external_dataset[i-m])
+cleaned_groc = []
+print(dict_orders_orig)
+count = 0
+ns = list(network.nodes)
+N = len(ns)
+for i in ns:
+    count += 1
+    print(count, "/", N)
+    i2 = ((dict_orders_orig.get(int(i))))#int(i)
+    m = int(m)
+    if i2 <= m:
+        integrated_dataset.append(dataset[i2])
+    if i2 > m:
+        integrated_dataset.append(external_dataset[i2-m])
+        cleaned_groc.append(external_dataset[i2 - m])
 
 my_df = pd.DataFrame(integrated_dataset)
-my_df.to_csv('{0}/integrated_network.csv'.format(data_path), index=False, header=False)
+my_df.to_csv('{0}/integrated_train_network.csv'.format(data_path), index=False, header=False)
+
+
+my_df2 = pd.DataFrame(cleaned_groc)
+my_df2.to_csv('{0}/cleaned_groc_network.csv'.format(data_path), index=False, header=False)
